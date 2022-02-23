c3824e06705833e2bf2992091d4a72f02d06abac
André Luís Torres Pereira <andre.pereira@olist.com>
2022-02-23 20:26:55 -0300

    Setup New Routes

--- routes/routes.go
+++ routes/routes.go
@@ -14,11 +14,13 @@ import (
 func HandleResquest() {
 	r := mux.NewRouter()
 	r.Use(middleware.ContentTypeMiddleware)
+
 	r.HandleFunc("/", controllers.Home)
-	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
-	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
-	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
-	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
-	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
+	r.HandleFunc("/api/personalidades/todas", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/unidade/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades/add", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/del/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/upgrade/{id}", controllers.EditaPersonalidade).Methods("Put")
+
 	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
